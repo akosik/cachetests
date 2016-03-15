de709d61e9df795818bbd385ee6c097b7f926e90
Will Jones <thefreethinker127@gmail.com>
2016-03-15 13:15:59 -0700

    Create one passing test (on aled)

--- src/test.c
+++ src/test.c
@@ -4,15 +4,29 @@
 #include "testing.h"
 #include "cache.h"
 
+// Hash function borrowed from Alex Ledger's implementation
+uint64_t our_modified_jenkins(key_type key)
+{
+    // https://en.wikipedia.org/wiki/Jenkins_hash_function
+    uint32_t hash = *key;
+    hash += (hash << 10);
+    hash ^= (hash >> 6);
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+    return (uint64_t) hash;
+}
+
 void test_get_entry()
 {
-    cache_t cache = create_cache(256);
-    uint8_t key = 20;
-    uint64_t value = 420;
+    uint8_t key[2] = {'a', '\0'};
+    uint8_t value[6] = {10,11,12,13,14,15};
     uint32_t val_size = sizeof(value);
-    cache_set(cache, &key, &value, &val_size);
-    uint64_t result = cache_get(cache, &key, &val_size);
-    test(result == 420, "Can retrieve first entry.");
+    
+    cache_t cache = create_cache(100 * sizeof(value), &our_modified_jenkins);
+    cache_set(cache, key, value, val_size);
+    uint8_t *result = cache_get(cache, key, &val_size);
+    test(result[0] == 10, "Can retrieve first entry.");
 }
 
 int main(int argc, char *argv[])
